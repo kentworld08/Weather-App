30c4d5259dc3b6e009410da4e3a0039e2aacfc53
Jonathan Adebo <kent93226@gmail.com>
2025-05-22 00:35:24 +0100

    added a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { CountryWeatherData } from "./Components/CountryWeatherData";
+import { Navbar } from "./Components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <CountryWeatherData />
     </>
   );
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,9 @@
+export const Navbar = () => {
+  return (
+    <header className="fixed top-0 right-0 left-0 bg-white shadow-2xl px-5 z-10">
+      <nav className="flex justify-between items-center h-10">
+        <h1 className="font-medium ">Weather App</h1>
+      </nav>
+    </header>
+  );
+};
