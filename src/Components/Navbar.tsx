56991a82e94dd7029310daa9840a8a4d9135c733
Jonathan Adebo <kent93226@gmail.com>
2025-05-22 00:44:14 +0100

    added navbar weather icon

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -2,7 +2,10 @@ export const Navbar = () => {
   return (
     <header className="fixed top-0 right-0 left-0 bg-white shadow-2xl px-5 z-10">
       <nav className="flex justify-between items-center h-10">
-        <h1 className="font-medium ">Weather App</h1>
+        <div className="flex items-center gap-2">
+          <img src="/image.png" alt="" width={20} height={20} loading="lazy" />
+          <h1 className="font-medium">Weather</h1>
+        </div>
       </nav>
     </header>
   );
